7ccb141d30186cd270720b274331fea0de337b7b
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-10-15 21:23:57 -0400

    Complete

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bonjoviImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.bonjoviImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.bonjoviImageView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.4)
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        self.bonjoviImageView.translatesAutoresizingMaskIntoConstraints = false
         
-
+        UIView.animateKeyframes(withDuration: 0.3, delay: 0.0, options: .calculationModeLinear, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                print("start.00")
+                self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 0.8)
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5, animations: {
+                self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 3.0)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
+                self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 2.4)
+            })
+        
+            }, completion: nil)
+        self.view.layoutIfNeeded()
     }
 
 }
