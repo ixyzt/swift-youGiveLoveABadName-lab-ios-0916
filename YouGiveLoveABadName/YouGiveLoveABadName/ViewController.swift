abb47499efa52d8c86f6961832534e04042da7c0
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-10-16 13:05:30 -0400

    Complete w/Reverse

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var bonjoviImageView: UIImageView!
+    
+    var animSwitch = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,24 +22,44 @@ class ViewController: UIViewController {
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         self.bonjoviImageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        UIView.animateKeyframes(withDuration: 0.3, delay: 0.0, options: .calculationModeLinear, animations: {
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
-                print("start.00")
-                self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 0.8)
-                self.view.layoutIfNeeded()
-            })
-            
-            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5, animations: {
-                self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 3.0)
-            })
-            
-            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
-                self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 2.4)
-            })
-        
-            }, completion: nil)
-        self.view.layoutIfNeeded()
+        switch animSwitch {
+        case true:
+            UIView.animateKeyframes(withDuration: 0.3, delay: 0.0, options: .calculationModeLinear, animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                    print("Forward")
+                    self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 0.8)
+                    self.view.layoutIfNeeded()
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5, animations: {
+                    self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 3.0)
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
+                    self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 2.4)
+                })
+                
+                }, completion: nil)
+            animSwitch = false
+        default:
+            UIView.animateKeyframes(withDuration: 0.3, delay: 0.0, options: .calculationModeLinear, animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25, animations: {
+                    self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 3.0)
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5, animations: {
+                    print("Reverse")
+                    self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 0.8)
+                    self.view.layoutIfNeeded()
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
+                    self.bonjoviImageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                })
+                
+                }, completion: nil)
+            animSwitch = true
+        }
     }
 
 }
